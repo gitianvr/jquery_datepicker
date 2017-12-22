5b66da50ac4407be658c563d27d670c0aa196696
Christian van Rensen <christian.van-rensen@diospi-suyana.org>
2017-12-22 13:13:17 -0500

    change to lazy initialization
    * don't append inline JS
    * instead: add class make_datepicker and store parameters in data-datepicker attributes
    * apply the formatting also to defaultDate, minDate, maxDate
    * perform value formatting in cases where the value was not specified explicitly but read from the object
    * define a default date format
    * remove datetimepicker support (only datepicker)

--- lib/app/helpers/form_helper.rb
+++ lib/app/helpers/form_helper.rb
@@ -5,14 +5,8 @@ module JqueryDatepicker
     include ActionView::Helpers::JavaScriptHelper
 
     # Method that generates datepicker input field inside a form
-    def datepicker(object_name, method, options = {}, timepicker = false)
-      input_tag =  JqueryDatepicker::Tags.new(object_name, method, self, options)
-      dp_options, tf_options =  input_tag.split_options(options)
-      tf_options[:value] = input_tag.format_date(tf_options[:value], String.new(dp_options[:dateFormat])) if  tf_options[:value] && !tf_options[:value].empty? && dp_options.has_key?(:dateFormat)
-      html = input_tag.render
-      method = timepicker ? "datetimepicker" : "datepicker"
-      html += javascript_tag("jQuery(document).ready(function(){jQuery('##{input_tag.get_name_and_id(tf_options.stringify_keys)["id"]}').#{method}(#{dp_options.to_json})});")
-      html.html_safe
+    def datepicker(object_name, method, options = {})
+      JqueryDatepicker::Tags.new(object_name, method, self, options).render
     end
   end
 
@@ -32,16 +26,49 @@ class JqueryDatepicker::Tags < ActionView::Helpers::Tags::TextField
 
   FORMAT_REPLACEMENTES = { "yy" => "%Y", "mm" => "%m", "dd" => "%d", "d" => "%-d", "m" => "%-m", "y" => "%y", "M" => "%b"}
 
+  def self.default_dp_options
+    {
+        dateFormat: 'dd-mm-yy'
+    }
+  end
+
   # Extending ActionView::Helpers::Tags module to make Rails build the name and id
   # Just returns the options before generate the HTML in order to use the same id and name (see to_input_field_tag mehtod)
+  def initialize(object_name, method, template_object, options)
+    dp_options, tf_options = split_options(options)
 
-  def get_name_and_id(options = {})
-    add_default_name_and_id(options)
-    options
+    # default dp options
+    dp_options.reverse_merge!(self.class.default_dp_options)
+
+
+    # format value to match date format
+    dateFormat = dp_options[:dateFormat]
+    tf_options[:value] = format_date(tf_options[:value], dateFormat) if tf_options[:value] && !tf_options[:value].empty? && dp_options.has_key?(:dateFormat)
+
+    # format dp_options holding date
+    dp_options.merge!(dp_options.slice(*date_datepicker_options).transform_values{|v|format_date(v, dateFormat)})
+
+    # add class make_datepicker
+    tf_options[:class] = (Array.wrap(tf_options[:class]) + [:make_datepicker]).join(' ')
+
+    # transmit dp options in data field of tf options, permitting caller to overwrite values
+    tf_options[:data] = dp_options.transform_keys{|k|"datepicker-#{k.to_s.underscore.dasherize}"}.deep_merge(tf_options.fetch(:data){{}})
+
+    super(object_name, method, template_object, tf_options)
+  end
+
+  def render
+    super do |options|
+      options['value'] = format_date(options['value'], options['data']['datepicker-date-format'])
+    end
   end
 
   def available_datepicker_options
     [:disabled, :altField, :altFormat, :appendText, :autoSize, :buttonImage, :buttonImageOnly, :buttonText, :calculateWeek, :changeMonth, :changeYear, :closeText, :constrainInput, :currentText, :dateFormat, :dayNames, :dayNamesMin, :dayNamesShort, :defaultDate, :duration, :firstDay, :gotoCurrent, :hideIfNoPrevNext, :isRTL, :maxDate, :minDate, :monthNames, :monthNamesShort, :navigationAsDateFormat, :nextText, :numberOfMonths, :prevText, :selectOtherMonths, :shortYearCutoff, :showAnim, :showButtonPanel, :showCurrentAtPos, :showMonthAfterYear, :showOn, :showOptions, :showOtherMonths, :showWeek, :stepMonths, :weekHeader, :yearRange, :yearSuffix, :readOnly, :value, :size, :id]
+  end
+
+  def date_datepicker_options
+    [:defaultDate, :minDate, :maxDate]
   end
 
   def split_options(options)
@@ -50,8 +77,20 @@ class JqueryDatepicker::Tags < ActionView::Helpers::Tags::TextField
   end
 
   def format_date(tb_formatted, format)
+    if tb_formatted.blank?
+      return nil
+    end
+
     new_format = translate_format(format)
-    Date.parse(tb_formatted).strftime(new_format)
+    case tb_formatted
+    when String
+      Date.parse(tb_formatted)
+    else
+      tb_formatted.to_date
+    end.strftime(new_format)
+
+  rescue ArgumentError
+    nil # or maybe tb_formatted?
   end
 
   # Method that translates the datepicker date formats, defined in (http://docs.jquery.com/UI/Datepicker/formatDate)
@@ -59,7 +98,7 @@ class JqueryDatepicker::Tags < ActionView::Helpers::Tags::TextField
   # This gem is not going to support all the options, just the most used.
 
   def translate_format(format)
-    format.gsub!(/#{FORMAT_REPLACEMENTES.keys.join("|")}/) { |match| FORMAT_REPLACEMENTES[match] }
+    format.gsub(/#{FORMAT_REPLACEMENTES.keys.join("|")}/) { |match| FORMAT_REPLACEMENTES[match] }
   end
 
   def self.field_type
